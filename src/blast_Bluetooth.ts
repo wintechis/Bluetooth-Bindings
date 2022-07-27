a3e5744cf54ec7d4c7d5d2c45bcd7463de99a02a
Rene Dorsch <rene.dorsch@iis.fraunhofer.de>
2022-07-27 18:56:20 +0200

    Changed BLE Library to node-ble

--- src/blast_Bluetooth.ts
+++ src/blast_Bluetooth.ts
@@ -1,34 +1,38 @@
-const noble = require('@abandonware/noble')
+const { createBluetooth } = require('node-ble');
 
- const connected_devices = [] as any
- const connected_macs = [] as any
+const { bluetooth, destroy } = createBluetooth();
+
+
+const connected_devices = [] as any
+const connected_macs = [] as any
 
 function delay(time: number) {
-    return new Promise(resolve => setTimeout(resolve, time));
-  }
+  return new Promise(resolve => setTimeout(resolve, time));
+}
 
 /**
  * Returns a paired bluetooth device by their id.
  * @param {BluetoothDevice.id} id identifier of the device to get.
  * @returns {BluetoothDevice} the bluetooth device with id.
  */
- export const getDeviceById = async function (id: any) {
-    return new Promise(async function(resolve,reject){
-        await noble.startScanningAsync([], false);
-        console.log("[binding-Bluetooth]", "Scanning started")
-        noble.on('discover', async (peripheral: any) => {
-            if (peripheral.address == id){
-                await noble.stopScanningAsync();
-                console.log("[binding-Bluetooth]","Device found")
-                resolve(peripheral)
-            }
-        })
-        // Wait 15 seconds if not requested device not found raise error
-        await delay(15000)
-        await noble.stopScanningAsync();
-        throw Error(`Bluetooth device ${id} wasn't found.`)
+export const getDeviceById = async function (id: any) {
+  return new Promise(async function (resolve, reject) {
 
-    })
+    // get bluetooth adapter
+    const adapter = await bluetooth.defaultAdapter();
+
+    await adapter.startDiscovery();
+    console.log("[binding-Bluetooth]", "Scanning started")
+
+    const device = await adapter.waitDevice(id);
+    resolve(device);
+
+    // Wait 15 seconds if not requested device not found raise error
+    // Does it make sense? Shouldnt it be a Race
+    await delay(15000)
+    throw Error(`Bluetooth device ${id} wasn't found.`)
+
+  })
 };
 
 /**
@@ -36,38 +40,45 @@ function delay(time: number) {
  * @param {BluetoothDevice.id} id identifier of the device to connect to.
  * @return {Promise<Object>} representation of the complete request with response.
  */
- const connect = async function (id: string) {
-    try {
-      if (connected_macs.includes(id)){
-        console.log("[binding-Bluetooth]",`Device ${id} already connected`, 'Bluetooth');
-        return connected_devices[connected_macs.indexOf(id)]
-      }
-      else{
-        const device = await getDeviceById(id) as any;
-        console.log("[binding-Bluetooth]",`Connecting to ${id}`, 'Bluetooth');
-        await device.connectAsync();
-        connected_devices.push(device)
-        connected_macs.push(id)
-        return device
-      }  
-    } catch (error) {
-        throw Error(`Error connecting to Bluetooth device ${id}`);
-        console.error(error);
+const connect = async function (id: string) {
+  try {
+    if (connected_macs.includes(id)) {
+      console.log("[binding-Bluetooth]", `Device ${id} already connected`, 'Bluetooth');
+      return connected_devices[connected_macs.indexOf(id)]
     }
+    else {
+      const device = await getDeviceById(id) as any;
+      console.log("[binding-Bluetooth]", `Connecting to ${id}`, 'Bluetooth');
+      await device.connect();
+      connected_devices.push(device)
+      connected_macs.push(id)
+      return device
+    }
+  } catch (error) {
+    throw Error(`Error connecting to Bluetooth device ${id}`);
+    console.error(error);
+  }
 };
 
 
-async function get_service(peripheral: any, serviceUUID: any){
-    let serviceUUIDwo = serviceUUID.split('-').join('')
-    const services = await peripheral.discoverServicesAsync([]);
+async function get_service(peripheral: any, serviceUUID: string): Promise<any> {
+  let serviceUUIDwo = serviceUUID.toLowerCase();
 
-    // Get service
-    for (let i = 0; i < services.length; i++) {
-        if (services[i].uuid == serviceUUIDwo){
-            return services[i]
-        }
+  const gattServer = await peripheral.gatt()
+  const services = await gattServer.services();
+
+  let service = undefined;
+  // Get service
+  services.forEach(async (serviceID: any) => {
+    if (serviceID == serviceUUIDwo) {
+      service = gattServer.getPrimaryService(serviceID);
     }
-    throw Error("Service not found!")
+  });
+  if (service != undefined){
+    return service;
+  }
+
+  throw Error("Service not found!")
 }
 
 /**
@@ -78,40 +89,42 @@ async function get_service(peripheral: any, serviceUUID: any){
  * @returns {Promise<BluetoothRemoteGATT>} A BluetoothRemoteGATTService object.
  */
 const getPrimaryService = async function (id: any, serviceUUID: any) {
-    const device = await connect(id);
-    let service;
-    try {
-        //const thingsLog = getThingsLog();
-        console.log("[binding-Bluetooth]",
-            `Getting primary service ${serviceUUID}`,
-            'Bluetooth',
-            id
-        );
-        service = await get_service(device, serviceUUID);
-        console.log("[binding-Bluetooth]",
-            `Got primary service ${serviceUUID}`,
-            'Bluetooth',
-            id
-        );
-    } catch (error) {
-        console.error(error);
-        throw new Error(`No Services Matching UUID ${serviceUUID} found in Device.`);
-    }
-    return service;
+  const device = await connect(id);
+  let service;
+  try {
+    //const thingsLog = getThingsLog();
+    console.log("[binding-Bluetooth]",
+      `Getting primary service ${serviceUUID}`,
+      'Bluetooth',
+      id
+    );
+    service = await get_service(device, serviceUUID);
+    console.log("[binding-Bluetooth]",
+      `Got primary service ${serviceUUID}`,
+      'Bluetooth',
+      id
+    );
+  } catch (error) {
+    console.error(error);
+    throw new Error(`No Services Matching UUID ${serviceUUID} found in Device.`);
+  }
+  return service;
 };
 
 
-async function get_char(service: any, characteristicUUID: any){
-    let characteristicUUIDwo = characteristicUUID.split('-').join('')
+async function get_char(service: any, characteristicUUID: String) : Promise<any> {
+  let characteristicUUIDwo = characteristicUUID.toLowerCase()
 
-    const characteristics = await service.discoverCharacteristicsAsync([]);
-    
-    for (let i = 0; i < characteristics.length; i++) {
-        if(characteristics[i].uuid == characteristicUUIDwo){
-
-            return characteristics[i]
-        }
+  const characteristics = await service.characteristics();
+  let characteristic = undefined;
+  characteristics.forEach((characteristicID : any) => {
+    if (characteristicID == characteristicUUIDwo){
+      characteristic = service.getCharacteristic(characteristicID);
     }
+  });
+
+  return characteristic;
+ 
 }
 
 /**
@@ -124,33 +137,33 @@ async function get_char(service: any, characteristicUUID: any){
  * @returns {Promise<BluetoothRemoteGATTCharacteristic>} A BluetoothRemoteGATTCharacteristic object.
  */
 export const getCharacteristic = async function (
-    id: any,
-    serviceUUID: any,
-    characteristicUUID: any
+  id: any,
+  serviceUUID: any,
+  characteristicUUID: any
 ) {
-    const service = await getPrimaryService(id, serviceUUID);
-    if (!service) {
-        return;
-    }
-    let characteristic;
-    try {
-        //const thingsLog = getThingsLog();
-        console.log("[binding-Bluetooth]",
-            `Getting characteristic ${characteristicUUID} from service ${serviceUUID}`,
-            'Bluetooth',
-            id
-        );
-        characteristic = await get_char(service, characteristicUUID);
-        console.log("[binding-Bluetooth]",
-            `Got characteristic ${characteristicUUID} from service ${serviceUUID}`,
-            'Bluetooth',
-            id
-        );
-    } catch (error) {
-        console.error(error);
-        throw new Error('The device is not compatible with the connected block.');
-    }
-    return characteristic;
+  const service: any = await getPrimaryService(id, serviceUUID);
+  if (!service) {
+    return;
+  }
+  let characteristic;
+  try {
+    //const thingsLog = getThingsLog();
+    console.log("[binding-Bluetooth]",
+      `Getting characteristic ${characteristicUUID} from service ${serviceUUID}`,
+      'Bluetooth',
+      id
+    );
+    characteristic = await get_char(service, characteristicUUID);
+    console.log("[binding-Bluetooth]",
+      `Got characteristic ${characteristicUUID} from service ${serviceUUID}`,
+      'Bluetooth',
+      id
+    );
+  } catch (error) {
+    console.error(error);
+    throw new Error('The device has not the specified characteristic.');
+  }
+  return characteristic;
 };
 
 /**
@@ -162,31 +175,31 @@ export const getCharacteristic = async function (
  * @public
  */
 export const read = async function (id: any, serviceUUID: any, characteristicUUID: any) {
-    const characteristic = await getCharacteristic(
-        id,
-        serviceUUID,
-        characteristicUUID
+  const characteristic: any = await getCharacteristic(
+    id,
+    serviceUUID,
+    characteristicUUID
+  );
+  try {
+    //const thingsLog = getThingsLog();
+    console.log("[binding-Bluetooth]",
+      `Invoke ReadValue on characteristic ${characteristicUUID}` +
+      ` from service ${serviceUUID}`,
+      'Bluetooth',
+      id
     );
-    try {
-        //const thingsLog = getThingsLog();
-        console.log("[binding-Bluetooth]",
-            `Invoke ReadValue on characteristic ${characteristicUUID}` +
-            ` from service ${serviceUUID}`,
-            'Bluetooth',
-            id
-        );
-        const value = await characteristic.readAsync();
-        console.log("[binding-Bluetooth]",
-            `Finished ReadValue on characteristic ${characteristicUUID}` +
-            ` from service ${serviceUUID} - value: ${value.toString()}`,
-            'Bluetooth',
-            id
-        );
-        return value;
-    } catch (error) {
-        console.error(error);
-        throw new Error(`Error reading from Bluetooth device ${id}`);
-    }
+    const value: any = await characteristic.readValue();
+    console.log("[binding-Bluetooth]",
+      `Finished ReadValue on characteristic ${characteristicUUID}` +
+      ` from service ${serviceUUID} - value: ${value.toString()}`,
+      'Bluetooth',
+      id
+    );
+    return value;
+  } catch (error) {
+    console.error(error);
+    throw new Error(`Error reading from Bluetooth device ${id}`);
+  }
 };
 
 /**
@@ -198,11 +211,11 @@ export const read = async function (id: any, serviceUUID: any, characteristicUUI
  * @public
  */
 export const readInt = async function (id: any, serviceUUID: any, characteristicUUID: any) {
-    let buffer = await read(id, serviceUUID, characteristicUUID);
-    const length = buffer.length;
-    const result = buffer.readIntLE(0, length);
-  
-    return result;
+  let buffer = await read(id, serviceUUID, characteristicUUID);
+  const length = buffer.length;
+  const result = buffer.readIntLE(0, length);
+
+  return result;
 };
 
 /**
@@ -213,7 +226,7 @@ export const readInt = async function (id: any, serviceUUID: any, characteristic
  * @returns {string} the value of the characteristic.
  * @public
  */
- export const readUInt = async function (id: any, serviceUUID: any, characteristicUUID: any) {
+export const readUInt = async function (id: any, serviceUUID: any, characteristicUUID: any) {
   let buffer = await read(id, serviceUUID, characteristicUUID);
   const length = buffer.length;
   const result = buffer.readUIntLE(0, length);
@@ -229,48 +242,48 @@ export const readInt = async function (id: any, serviceUUID: any, characteristic
  * @param {string} value hex value to write.
  * @returns {Promise<void>} A Promise to void.
  */
- export const writeWithoutResponse = async function (
-    id: any,
-    serviceUUID: any,
-    characteristicUUID: any,
-    value: any
-  ) {
-    const characteristic = await getCharacteristic(
-      id,
-      serviceUUID,
-      characteristicUUID
+export const writeWithoutResponse = async function (
+  id: any,
+  serviceUUID: any,
+  characteristicUUID: any,
+  value: any
+) {
+  const characteristic: any = await getCharacteristic(
+    id,
+    serviceUUID,
+    characteristicUUID
+  );
+  if (!characteristic) {
+    return;
+  }
+
+  // If value is a string, convert it to an ArrayBuffer.
+  if (typeof value === 'string') {
+    value = hexStringToArrayBuffer(value);
+  }
+
+  try {
+    //const thingsLog = getThingsLog();
+    console.log("[binding-Bluetooth]",
+      'Invoke WriteValueWithoutResponse on characteristic ' +
+      `${characteristicUUID} with value ${value.toString()}`,
+      'Bluetooth',
+      id
     );
-    if (!characteristic) {
-      return;
-    }
-  
-    // If value is a string, convert it to an ArrayBuffer.
-    if (typeof value === 'string') {
-      value = hexStringToArrayBuffer(value);
-    }
-  
-    try {
-      //const thingsLog = getThingsLog();
-      console.log("[binding-Bluetooth]",
-        'Invoke WriteValueWithoutResponse on characteristic ' +
-          `${characteristicUUID} with value ${value.toString()}`,
-        'Bluetooth',
-        id
-      );
-      await characteristic.writeAsync(value, true);
-      console.log("[binding-Bluetooth]",
-        'Finished WriteValueWithoutResponse on characteristic ' +
-          `${characteristicUUID} with value ${value.toString()}`,
-        'Bluetooth',
-        id
-      );
-    } catch (error) {
-      const errorMsg =
-        'Error writing to Bluetooth device.\nMake sure the device is compatible with the connected block.';
-      console.error(error);
-      throw new Error(errorMsg);
-    }
-  };
+    await characteristic.writeValue(value, {offset: 0, type: 'command'});
+    console.log("[binding-Bluetooth]",
+      'Finished WriteValueWithoutResponse on characteristic ' +
+      `${characteristicUUID} with value ${value.toString()}`,
+      'Bluetooth',
+      id
+    );
+  } catch (error) {
+    const errorMsg =
+      'Error writing to Bluetooth device.\nMake sure the device is compatible with the connected block.';
+    console.error(error);
+    throw new Error(errorMsg);
+  }
+};
 
 
 /**
@@ -281,89 +294,89 @@ export const readInt = async function (id: any, serviceUUID: any, characteristic
  * @param {string} value hex value to write.
  * @returns {Promise} representation of the complete request with response.
  */
- export const writeWithResponse = async function (
-    id: any,
-    serviceUUID: any,
-    characteristicUUID: any,
-    value: any
-  ) {
-    const characteristic = await getCharacteristic(
-      id,
-      serviceUUID,
-      characteristicUUID
+export const writeWithResponse = async function (
+  id: any,
+  serviceUUID: any,
+  characteristicUUID: any,
+  value: any
+) {
+  const characteristic: any = await getCharacteristic(
+    id,
+    serviceUUID,
+    characteristicUUID
+  );
+  if (!characteristic) {
+    return;
+  }
+
+  // If value is a string, convert it to an ArrayBuffer.
+  if (typeof value === 'string') {
+    value = hexStringToArrayBuffer(value);
+  }
+
+  try {
+    console.log("[binding-Bluetooth]",
+      'Invoke WriteValueWithResponse on characteristic ' +
+      `${characteristicUUID} with value ${value.toString()}`,
+      'Bluetooth',
+      id
     );
-    if (!characteristic) {
-      return;
-    }
-  
-    // If value is a string, convert it to an ArrayBuffer.
-    if (typeof value === 'string') {
-      value = hexStringToArrayBuffer(value);
-    }
-  
-    try {
-      console.log("[binding-Bluetooth]",
-        'Invoke WriteValueWithResponse on characteristic ' +
-          `${characteristicUUID} with value ${value.toString()}`,
-        'Bluetooth',
-        id
-      );
-      await characteristic.writeAsync(value, false);
-      console.log("[binding-Bluetooth]",
-        'Finished WriteValueWithResponse on characteristic ' +
-          `${characteristicUUID} with value ${value.toString()}`,
-        'Bluetooth',
-        id
-      );
-    } catch (error) {
-      const errorMsg =
-        'Error writing to Bluetooth device.\nMake sure the device is compatible with the connected block.';
-      console.error(error);
-      throw new Error(errorMsg);
-    }
-  };
+    await characteristic.writeValue(value, {offset: 0, type: 'request'});
+    console.log("[binding-Bluetooth]",
+      'Finished WriteValueWithResponse on characteristic ' +
+      `${characteristicUUID} with value ${value.toString()}`,
+      'Bluetooth',
+      id
+    );
+  } catch (error) {
+    const errorMsg =
+      'Error writing to Bluetooth device.\nMake sure the device is compatible with the connected block.';
+    console.error(error);
+    throw new Error(errorMsg);
+  }
+};
 
-  /**
- * Convert a hex string to an ArrayBuffer.
- *
- * @param {string} hexString - hex representation of bytes
- * @return {ArrayBuffer} - The bytes in an ArrayBuffer.
- */
+/**
+* Convert a hex string to an ArrayBuffer.
+*
+* @param {string} hexString - hex representation of bytes
+* @return {ArrayBuffer} - The bytes in an ArrayBuffer.
+*/
 const hexStringToArrayBuffer = function (hexString: any) {
-    // remove the leading 0x
-    hexString = hexString.replace(/^0x/, '');
-    
-    // check for some non-hex characters
-    const bad = hexString.match(/[G-Z\s]/i);
-    if (bad) {
-      console.log('WARNING: found non-hex characters', bad, 'trying to correct');
-    }
-    
-    hexString = hexString.replace(/[^\w\s]/gi, '') // special char and white space
-    hexString = hexString.replace(/[G-Z\s]/i, '')
-  
-    // ensure even number of characters
-    if (hexString.length % 2 !== 0) {
-      hexString = '0' + hexString;
-    }
+  // remove the leading 0x
+  hexString = hexString.replace(/^0x/, '');
 
-    hexString = Buffer.from(hexString, "hex")
-    
-    return hexString
-  };
+  // check for some non-hex characters
+  const bad = hexString.match(/[G-Z\s]/i);
+  if (bad) {
+    console.log('WARNING: found non-hex characters', bad, 'trying to correct');
+  }
+
+  hexString = hexString.replace(/[^\w\s]/gi, '') // special char and white space
+  hexString = hexString.replace(/[G-Z\s]/i, '')
+
+  // ensure even number of characters
+  if (hexString.length % 2 !== 0) {
+    hexString = '0' + hexString;
+  }
+
+  hexString = Buffer.from(hexString, "hex")
+
+  return hexString
+};
 
 
 export const tearDown = async function () {
-    for (const element of connected_devices) {
-        console.log("[binding-Bluetooth]","Disconnecting from Device:", element.address)
-        await element.disconnectAsync();
-      }
-    // Remove all items from connected_devices
-    connected_devices.length = 0
-    connected_macs.length = 0
+  for (const element of connected_devices) {
+    console.log("[binding-Bluetooth]", "Disconnecting from Device:", element)
+    await element.disconnect();
+  }
+  // Remove all items from connected_devices
+  connected_devices.length = 0
+  connected_macs.length = 0
 }
 
-export const get_td_from_device = async function(MAC: string) {
+export const get_td_from_device = async function (MAC: string) {
   const WoT_Service = "1fc8f811-0000-4e89-8476-e0b2dad3179b"
   const td_Char = "2ab6"
 
@@ -372,6 +385,6 @@ export const get_td_from_device = async function(MAC: string) {
   await tearDown()
 
   return value.toString()
-  
+
 }
 
